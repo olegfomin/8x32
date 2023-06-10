bd6ad34f58c56cb61703b5db3558b847e68c9ea2
Oleg Fomin <oleg.fomin@16x32>
2023-06-09 20:42:48 -0700

    Everythin seems to be working as needed, now callibration is needed

--- Command.py
+++ Command.py
@@ -1,124 +1,25 @@
 import time                                  #import the time module
 import piplates.MOTORplate as MOTOR          #import the MOTORplate module
 import queue
+import json
+import math
 
-
-# Implementing the command pattern that encapsulates all the dencapsulate
-# in an object all the data required for performing a given action (command), 
-# including what method to call, the method's arguments, and the object to which the method belongs
-
+# Implementing the command pattern that encapsulates all the name
+# and single value for a command. Then all the commands are
+# gathered together inside a Record queue and then this record is
+# being played by RecordPlayer that executes the command by sending the signals
+# to the motor controller. All the motor controller commands are 
+# pure functions as the OOP behaved very strangely overlaping commands
+# with each other
+  
 CLOCK_WISE = 1
 COUNTER_CLOCK_WISE = -1
 TURN_COEFFICIENT_PIVOT = 0.005 # This value needs to be resolved empirically or it may be even a function
 TURN_COEFFICIENT_SIMPLE = 0.004
-ANGLE2TIME=0.002
+ANGLE2TIME=0.0028
 CALLIBRATION_LEFT  = 1.0
 CALLIBRATION_RIGHT = 1.0
-DISTANCE2TIME=0.001
-
-
-class Motor():
-	exPower=0 # previous power
-	exDirection=0 # previous direction
-	wasUsedOnce = False
-	# Takes a motor number from 1 to 4 and acceleration in seconds
-	def __init__(self, number, acceleration):
-		if(number <=0 or number > 4): raise Exception("The motor number should be between 1 and 4")
-		self.number=number # The engine number from 1 to 4
-		self.acceleration = acceleration
-		
-	def resolveDirection(self, direction):
-		if(direction > 0): # Here it is supposed to be the othe way around but I am too lazy to move wiring
-			 return 'cww'
-		else:
-			 return 'cw'
-			 
-	def delay(self, power, direction):
-		print(power)
-		if(self.wasUsedOnce and (power != self.exPower or direction != self.exDirection)):
-			time.sleep(0.1) # Allows some time for MOTOR.dcCONFIG(...) to do its business otherwise it behaves funky  
-
-			
-	# power is from 0 to 100, direction clockwise - positive number; counter clock wise - negative number
-	def move(self, power, direction):
-		if(direction == 0): raise Exception("The motor direction must be either negative or positive")
-		channel = 0
-		motorNumber = self.number
-		directionAsStr = self.resolveDirection(direction)
-		motorPower = power
-		motorAcceleration = self.acceleration
-		self.delay(power, direction)
-		MOTOR.dcCONFIG(channel, self.number, directionAsStr, power, self.acceleration)
-		MOTOR.dcSTART(channel, self.number)
-        
-	def changeSpeed(self, newPower):    
-		MOTOR.dcSPEED(0,self.number,newPower) #change the power
-        
-	def brake(self):
-		MOTOR.dcSTOP(0,self.number) #stop the motor
-                
-	
-#The wheels 1,2 going one way and wheels 3,4 the oposite way
-#With the big battery on the top it does not work
-#It needs to be removed to show this behaviour with
-#the lighter machine  
-class PivotSteerCommand:
-	motor = [Motor(1, 2.1), Motor(2, 2.1), Motor(3, 2.1), Motor(4, 2.1)]  
-
-	def __init__(self, angle, direction): # angle value that can be from 0 to 180
-		self.duration = TURN_COEFFICIENT_PIVOT*angle
-		self.direction = direction
-		
-	def execute(self):
-		self.motor[0].move(100,self.direction)
-		self.motor[1].move(100,self.direction)
-		self.motor[2].move(100,-self.direction)
-		self.motor[3].move(100,-self.direction)
-		time.sleep(self.duration)
-		self.motor[3].brake()
-		self.motor[2].brake()
-		self.motor[1].brake()
-		self.motor[0].brake()
-		
-#moveStraightCommand = MoveStraightCommand(5000)
-#moveStraightCommand.execute()
-		
-		
-#pivotSteerCommand = PivotSteerCommand(20000, CLOCK_WISE)		
-#pivotSteerCommand.execute()
-
-# the wheels 1,2 goes full steam ahead and 3, 4 very slowly going ahead in order not to stall
-# the turn completely 		  		
-class SimpleTurnCommand:		  		
-	motor = [Motor(1, 2.1), Motor(2, 2.1), Motor(3, 2.1), Motor(4, 2.1)]
-	
-	def __init__(self, angle, direction): # angle value that can be from 0 to 180
-		self.duration = TURN_COEFFICIENT_SIMPLE*angle
-		self.direction = direction
-		self.multiplier12=0
-		self.multiplier34=0
-		if(direction > 0): # clock wise
-			self.multiplier12=1
-			self.multiplier34=0.08
-		if(direction < 0): # counter clock wise
-			self.multiplier12=0.08
-			self.multiplier34=1
-		
-	def execute(self):
-		self.motor[0].move(100*self.multiplier12,self.direction)
-		self.motor[1].move(100*self.multiplier12,self.direction)
-		time.sleep(10)
-		self.motor[2].move(100*self.multiplier34,self.direction)
-		self.motor[3].move(100*self.multiplier34,self.direction)
-		time.sleep(self.duration)
-		self.motor[3].brake()
-		self.motor[2].brake()
-		self.motor[1].brake()
-		self.motor[0].brake()
-		
-		
-simpleTurnCommand = SimpleTurnCommand(15000, CLOCK_WISE)
-#simpleTurnCommand.execute()
+DISTANCE2TIME=0.0018
 
 def m1Start(direction, power) :
 	callibratedPower = CALLIBRATION_LEFT*power 
@@ -175,9 +76,6 @@ def turnLeftPivot(angle):
   m3Stop()
   m4Stop()
   
-
-#turnRightPivot(36)
-
 def turnRightSimple(angle): 
   m1Start('ccw', 18)
   m2Start('ccw', 18)
@@ -231,9 +129,18 @@ time.sleep(0.1)
 goForward(5000)'''
 
 class Command:
+	name = 'none'
+	value = 0
 	def __init__(self, name, value):
 		self.name = name
 		self.value = value
+	def __init__(self, name):
+		self.name = name
+		
+	def __str__(self):
+		return 'Command {name="'+self.name+'", value="'+str(self.value)+'"}'
+
+		
 
 
 class Record:
@@ -249,31 +156,123 @@ class Record:
 		return 	self.commandsQueue.empty()
 		
 class RecordPlayer:
+	DELAY_BETWEEN_COMMANDS = 1.0
+	def __init__(self, aRecord):
+		self.record=aRecord
 		
-	def play(self, record):
-		while not record.isEmpty():
-			command = record.getCommand()
+	def play(self):
+		while(not self.record.isEmpty()):
+			command = self.record.getCommand()
+			print(command)
 			if(command.name == 'goBackward'):
 				goBackward(command.value)
-				time.sleep(0.1)
+				time.sleep(self.DELAY_BETWEEN_COMMANDS) # This sleep is very important as it allows to switch motor controller context I guess
 			elif(command.name == 'goForward'):
 				goForward(command.value)
-				time.sleep(0.1)
+				time.sleep(self.DELAY_BETWEEN_COMMANDS)
 			elif(command.name == 'turnLeftPivot'):
 				turnLeftPivot(command.value)
-				time.sleep(0.1)
+				time.sleep(self.DELAY_BETWEEN_COMMANDS)
 			elif(command.name == 'turnRightPivot'):
 				turnRightPivot(command.value)
-				time.sleep(0.1)
+				time.sleep(self.DELAY_BETWEEN_COMMANDS)
 			elif(command.name == 'turnRightSimple'):
 				turnRightSimple(command.value)
-				time.sleep(0.1)
+				time.sleep(self.DELAY_BETWEEN_COMMANDS)
 			elif(command.name == 'turnLeftSimple'):
 				turnLeftSimple(command.value)
-				time.sleep(0.1)
+				time.sleep(self.DELAY_BETWEEN_COMMANDS)
+			elif(command.name == 'fire'):
+				#empty command for now
+				time.sleep(self.DELAY_BETWEEN_COMMANDS)
+			elif(command.name == 'none'): # in case the turn is not neccessary it is still good to keep a command in the Record
+				time.sleep(self.DELAY_BETWEEN_COMMANDS)	
 			else:
 				raise('Unknown command: '+ command.value)
 				
+
+				
+class TapeRecorder:
+	def __init__(self, aRecord):
+		self.record=aRecord
+
+	#Json Sample: {"Command":"coords","Payload":[{"x":330,"y":946},{"x":397.40714285714284,"y":644.7902719550473}],"token":"dd7bac0f-b441-47a7-8aa0-91e656527849"}
+	def addRecord(self, jsonAsObj):
+		startFinishArr = jsonAsObj["Payload"];
+		startXY = startFinishArr[0]
+		xs = startXY["x"]
+		ys = startXY["y"]
+		finishXY = startFinishArr[1]
+		xf = finishXY["x"]
+		yf = finishXY["y"]
+		
+		width = xf-xs;
+		length = yf - ys;
+		
+		moveCommandName = 'none' 
+		if(length < -30):
+			moveCommandName = 'goForward'
+		elif(length > 30):
+			moveCommandName = 'goBackward'
+			
+		moveCommand = Command(moveCommandName)	
+		 
+		turnCommandName = 'none' # the target is straight ahead, there no need to turn
+		if(width > 23):
+			if(moveCommandName == 'goBackward'): # Swapping right and letf turns if going backward
+				turnCommandName = 'turnLeftPivot'
+			else:		
+				turnCommandName = 'turnRightPivot'
+		elif(width < -23):
+			if(moveCommandName == 'goBackward'): # Swapping right and letf turns if going backward
+				turnCommandName = 'turnRightPivot'
+			else:
+				turnCommandName = 'turnLeftPivot'
+			
+		turnCommand = Command(turnCommandName)
+		
+		diagonal = abs(length) # in case we go straight	ahead
+		if(turnCommandName != 'none'):	
+		    diagonal = math.sqrt(width**2+length**2) # calculating how long shall we go if there is an angle
+		    absWidth = abs(width)
+		    sine = absWidth/diagonal 
+		    angleInRadians = math.asin(sine)	
+		    angleInDegrees = math.degrees(angleInRadians)
+		    turnCommand.value = angleInDegrees
+		    
+		if(moveCommandName != 'none'):
+			 moveCommand.value = diagonal*3.25; # Turning pixels in millimeters
+		
+		turnBackCommandName = 'none'	 
+		if(turnCommandName == 'turnLeftPivot'):
+			turnBackCommandName = 'turnRightPivot'
+		elif(turnCommandName == 'turnRightPivot'):
+			turnBackCommandName = 'turnLeftPivot'
+		
+		turnBackCommand = Command(turnBackCommandName)
+		turnBackCommand.value = turnCommand.value
+		
+		self.record.addCommand(turnCommand)
+		self.record.addCommand(moveCommand)
+		self.record.addCommand(turnBackCommand)
+		# fire command should be here maybe but not too sure
+		
+		
+'''		
+	
+jsonAsString = '{"Command":"coords","Payload":[{"x":330,"y":946},{"x":397.40714285714284,"y":644.7902719550473}],"token":"dd7bac0f-b441-47a7-8aa0-91e656527849"}'
+jsonAsStr1= '{"Command":"coords","Payload":[{"x":330,"y":946},{"x":380.55,"y":670.0761649728922}],"token":"74331cb9-6a7f-4187-aeb1-55cfa6c3b158"}'
+jsonAsObj = json.loads(jsonAsStr1)
+
+record = Record();
+tapeRecorder = TapeRecorder(record)
+tapeRecorder.addRecord(jsonAsObj)
+recordPlayer = RecordPlayer(record)
+recordPlayer.play();
+
+'''
+
+'''				
 record = Record()				
 record.addCommand(Command('goBackward', 10000))
 record.addCommand(Command('turnLeftPivot', 60))
@@ -284,7 +283,8 @@ record.addCommand(Command('turnRightSimple', 80))
 record.addCommand(Command('turnLeftSimple', 35))
 
 recordPlayer = RecordPlayer()
-recordPlayer.play(record)
+
+'''
 
 
 
