230b895750461564b7668929daf1e55596fee5f6
Oleg Fomin <oleg.fomin@16x32>
2023-06-16 19:20:00 -0700

    Extracting the common part for hall effect sensors

---
+++ HallEffectInterruption.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import threading
+import time
+
+
+class HallEffectInterruption:
+	PRINT_INTERVAL = 5.5
+	
+	gpioNumber = 0
+	pressedCounter=0
+	releasedCounter=0
+	wasCalled = False
+	amIPressed = False
+	
+	def __init__(self, gpioNumber):
+		self.gpioNumber = gpioNumber
+		self.printerThreading = threading.Thread(target=self.printValues, daemon=True)
+		
+
+	def signal_handler(self, sig, frame):
+		GPIO.cleanup(self.gpioNumber)
+		sys.exit(0)
+	def hallSensorCallback(self, channel):
+		if(not self.wasCalled):
+			self.printerThreading.start();
+			self.wasCalled = True
+		if(not GPIO.input(self.gpioNumber)):
+			self.amIPressed = True
+			self.pressedCounter+=1
+		else:
+			self.amIPressed = False
+			self.releasedCounter+=1
+				
+	def printValues(self):
+		while True:
+			print("SignalOn detected!")
+			print(self.pressedCounter)
+			print("SignalOff detected!")
+			print(self.releasedCounter)
+			print(self.amIPressed)
+			time.sleep(self.PRINT_INTERVAL)
+
+
--- Motor2Interrupt.py
+++ Motor2Interrupt.py
@@ -2,19 +2,16 @@ import signal
 import sys
 import RPi.GPIO as GPIO
 BUTTON_GPIO = 24
-def signal_handler(sig, frame):
-    GPIO.cleanup()
-    sys.exit(0)
-def button_callback(channel):
-    if not GPIO.input(BUTTON_GPIO):
-        print("Button pressed!")
-    else:
-        print("Button released!")
+
+from HallEffectInterruption import HallEffectInterruption
+
 if __name__ == '__main__':
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+	hallEffectInterruption = HallEffectInterruption(BUTTON_GPIO)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     
-    GPIO.add_event_detect(BUTTON_GPIO, GPIO.BOTH, callback=button_callback, bouncetime=50)
+	GPIO.add_event_detect(BUTTON_GPIO, GPIO.BOTH, callback=hallEffectInterruption.hallSensorCallback, bouncetime=5)
     
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.pause()
+	signal.signal(signal.SIGINT, hallEffectInterruption.signal_handler)
+	signal.pause()
